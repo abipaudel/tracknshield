3b9b3002c20846c14c50097d9e0be448747bcbc2
Abi Paudel <paudel.abi@gmail.com>
2025-06-21 18:56:37 +0545

    Update AssetInventory.tsx
    Signed-off-by: Abi Paudel <paudel.abi@gmail.com>

--- src/components/AssetInventory.tsx
+++ src/components/AssetInventory.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NepaliRupeeIcon } from './components/NepaliRupeeIcon';  
+import { NepaliRupeeIcon } from '/components/NepaliRupeeIcon';  
 import { 
   Package, 
   Plus, 
